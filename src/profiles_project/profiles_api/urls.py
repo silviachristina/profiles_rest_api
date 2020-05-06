9ed8b239ea3d3069e9208ff45d4d0d613b762e4b
Silvia Christina Santos <silvia.christina@hotmail.com>
2020-05-06 02:47:50 -0300

    Added profile listing and creating feature

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 router = DefaultRouter() # viewset
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') # viewset
+router.register('profile', views.UserProfileViewSet) #when you registrer a model viewset you don't need to specify the base name django automatically
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
